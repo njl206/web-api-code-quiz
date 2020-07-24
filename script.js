1df0107eb1e078841e0da1f1402d635a859c7a22
Nick Landry <njlandry@gmail.com>
2020-07-24 15:59:58 -0700

    JS and HTML work

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let questions = [
     {
-        question: "what color is water?",
+        title: "what color is water?",
         choices: ["red", "blue", "green", "orange"],
         answer: "blue",
     }
 ]
 
+var timer = document.querySelector("#timer");
+var startQuizButton = document.querySelector("#startQuiz");
+var quiz = document.querySelector("#quiz");
+var scoreButton = document.querySelector("highScores");
+var clearScoreButton = document.querySelector("#clearScores");
+var retry = document.querySelector("#retry");
+
+
 let secondsLeft = 30;
 let currentQuestion = 0;
 let score = 0;
+
+startQuizButton.addEventListener("click", startQuiz);
+scoreButton.addEventListener("click", toggleScoreDisplay);
+clearScoreButton.addEventListener("click", clearScores);
+
+
+function toggleScoreDisplay(){
+    
+}
 
 function setTime() {
     var timerInterval = setInterval(function () {
@@ -31,8 +48,46 @@ function startQuiz() {
     setTime();
 }
 
-function displayQuestion(questionArray) {
-    for (var i = 0; i < questionArray.length; i++) {
-        console.log(questionArray[i].question)
+function displayQuestion() {
+   var questionList = questions[currentQuestion].choices.map((question)=> {
+       return `<button class="answerbutton" onclick="answerQuestion('${question}')"> ${question}</button>`;
+
+   });
+   quiz.innerHTML = `${questions[currentQuestion].title}<br>${questionList.join("")}`;
+    
+};
+
+function answerQuestion (selection){
+    if (questions[currentQuestion].answer === selection) {
+        score++;
+    } else {
+        secondsLeft -=10;
     }
-}
+    currentQuestion++;
+    if (currentQuestion === questions.length) {
+        return endGame()
+    }
+    displayQuestion();
+};
+
+function endGame (){
+    quiz.innerHTML = `
+    <p>You got ${score} of ${questions.length}</p>
+    <form onsubmit="handleScoreSave(event)">
+    <input type="text" placeholder="enter initials"></input> 
+    <input type="submit" value="save score"></input> 
+    </form>
+    `;
+
+    secondsLeft=0;
+    timer.innerHTML = `
+    <button id="retry" onclick="restartGame()"> retry? </button> 
+    `;
+};
+
+function restartGame (){
+    secondsLeft=30;
+    currentQuestion=0;
+    score=0;
+    startQuiz();
+};
